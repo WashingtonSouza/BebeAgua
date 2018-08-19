a2a6953c71870e6baef4fd9d8510bc7ee755d2e3
Washington Souza <thewashington.souza@gmail.com>
2018-08-18 23:40:27 -0300

    Criação de cabeçalho

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, ImageBackground, View } from 'react-native';
+import { StyleSheet, ImageBackground, View, Text } from 'react-native';
 
 export default class App extends Component {
   render() {
@@ -7,9 +7,22 @@ export default class App extends Component {
       <View style={styles.body}>
         <ImageBackground source={require('./images/waterbg.png')} style={styles.bgImage}>
           <View style={styles.infoArea}>
-            <View></View>
-            <View></View>
-            <View></View>
+
+            <View style={styles.area}>
+               <Text style={styles.areaTitulo}>Meta</Text>
+               <Text style={styles.areaDado}>2000 ml</Text>
+            </View>
+
+            <View style={styles.area}>
+               <Text style={styles.areaTitulo}>Consumido</Text>
+               <Text style={styles.areaDado}>1200 ml</Text>
+            </View>
+
+            <View style={styles.area}>
+               <Text style={styles.areaTitulo}>Status</Text>
+               <Text style={styles.areaDado}>Bom</Text>
+            </View>
+
           </View>
         </ImageBackground>
       </View>
@@ -28,6 +41,20 @@ const styles = StyleSheet.create({
   }, 
   infoArea:{
     flex:1,
-    flexDirection:'row'
+    flexDirection:'row',
+    paddingTop: 50
+  },
+  area:{
+     flex:1,
+     alignItems:'center'
+  },
+  areaTitulo:{
+     color:'#45b2fc'
+  },
+  areaDado:{
+     color:'#2b4274',
+     fontSize:15,
+     alignItems: 'center',
+     fontWeight:'bold'
   }
 });

4461f52e7db57ebcd184c4bb168d345e15904f23
Washington Souza <thewashington.souza@gmail.com>
2018-08-18 23:28:11 -0300

    Definição plano de fundo

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground, View } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.body}>
+        <ImageBackground source={require('./images/waterbg.png')} style={styles.bgImage}>
+          <View style={styles.infoArea}>
+            <View></View>
+            <View></View>
+            <View></View>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  
+  body:{
+    flex:1,
+    paddingTop:20
+  },
+  bgImage:{
+    flex:1,
+    width:null
+  }, 
+  infoArea:{
+    flex:1,
+    flexDirection:'row'
+  }
 });
